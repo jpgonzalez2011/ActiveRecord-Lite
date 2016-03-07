f1f3ccb4e385515a5fc6471a86f3ed0caff1b45c
JP <jp.gonzalez2011@gmail.com>
2016-03-07 18:32:14 -0500

    Remove bug in has many association functionality.

--- associatable.rb
+++ associatable.rb
@@ -30,7 +30,6 @@ class HasManyOptions < AssocOptions
     @foreign_key = options[:foreign_key] ||= "#{self_class_name.underscore}_id".to_sym
     @primary_key = options[:primary_key] ||= "id".to_sym
     @class_name = options[:class_name] ||= name.to_s.singularize.camelcase
-
   end
 end
 
@@ -41,7 +40,7 @@ module Associatable
       @foreign_key = options.send(:foreign_key)
       @class_name = options.model_class
       @primary_key = options.send(:primary_key)
-      p result = DBConnection.execute(<<-SQL).to_a.first
+      result = DBConnection.execute(<<-SQL).to_a.first
         SELECT
           #{options.table_name}.*
         FROM
@@ -58,7 +57,7 @@ module Associatable
   end
 
   def has_many(name, options = {})
-    options = HasManyOptions.new(name, "#{self.class}", options)
+    options = HasManyOptions.new(name, self.name, options)
     define_method(name) do
       @foreign_key = options.send(:foreign_key)
       @class_name = options.model_class
@@ -68,7 +67,6 @@ module Associatable
           #{options.table_name}.*
         FROM
           #{options.table_name}
-
         WHERE
           #{options.table_name}.#{@foreign_key} = #{self.attributes[:id]}
       SQL
@@ -76,7 +74,7 @@ module Associatable
       result.to_a.each do |result|
         final_results << options.model_class.new(result)
       end
-      p final_results
+      final_results
     end
   end
 
